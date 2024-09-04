4276a5c1da23a1357a03451290316f1691b30e69
vojtechpavel94 <vojtech.pavel@expertkom.cz>
2024-09-04 17:52:24 +0200

    replacing Autowired with constructors

--- src/main/java/cz/expertkom/sportFacilities/service/WeatherService.java
+++ src/main/java/cz/expertkom/sportFacilities/service/WeatherService.java
@@ -1,5 +1,6 @@
 package cz.expertkom.sportFacilities.service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -7,14 +8,18 @@ import org.springframework.web.client.RestTemplate;
 @Service
 @Slf4j
 public class WeatherService {
-    private final String API_KEY = "4ccef0bbdc4fb918a5344bbbd8d4932a";
+    private final String apiKey;
     private final RestTemplate restTemplate = new RestTemplate();
-
-    public String getWeather(double latitude, double longitude) {
+    public WeatherService() {
+        Dotenv dotenv = Dotenv.load();
+        this.apiKey = dotenv.get("WEATHER_API_KEY");
+    }
+    // Use the apiKey in your methods
+    public String getWeather ( double latitude, double longitude){
         log.info("#WS&gw01: getWeather called, latitude= {}, longitude= {}", latitude, longitude);
         String url = String.format(
                 "https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&appid=%s&units=metric&lang=cz",
-                latitude, longitude, API_KEY);
+                latitude, longitude, apiKey);
         return restTemplate.getForObject(url, String.class);
     }
 }
